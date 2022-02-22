d4dfec8377342cd77fb1b40c38b206fc06bbe7f7
nastyabekesheva <nastya.bekesheva@gmail.com>
2022-02-22 10:04:38 +0200

    dynamic memory matrix

---
+++ dynamic_matrix/dynamic_matrix/main.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  dynamic_matrix
+//
+//  Created by Nastya Bekesheva on 22.02.2022.
+//
+
+#include <iostream>
+#include "matrix.hpp"
+#include "matrix.cpp"
+
+int main() {
+    Matrix<int> matrix(3, 3);
+    cout << "Enter value:" << endl;
+    cin >> matrix;
+    cout << "Your matrix:" << endl;
+    cout << matrix;
+    matrix.getSize();
+    cout << endl;
+    matrix.getElement(3);
+    cout << endl;
+    Matrix<int> mat(matrix);
+    cout << "Adding two matrices" << endl << mat + matrix << endl;
+    cout << "Substracting two matrices" << endl << mat - matrix << endl;
+    cout << "Multiplying two matrices" << endl << mat * matrix << endl;
+    cout << "Adding value to matrix" << endl << matrix + 2 << endl;
+    cout << "Substracting value from matrix" << endl << matrix - 2 << endl;
+    cout << "Multiplying matrix by value" << endl << matrix * 2 << endl;
+    cout << "Dividing matrix by value" << endl << matrix / 2 << endl;
+    NegMatrix<int> m(3, 3);
+    cout << "Enter value:" << endl;
+    cin >> m;
+    cout << "Accessing element by negative index" << endl;
+    cout << m.accessItem(-1, -1) << endl;
+    return 0;
+}
---
+++ dynamic_matrix/dynamic_matrix/matrix.cpp
@@ -0,0 +1,239 @@
+//
+//  matrix.cpp
+//  dynamic_matrix
+//
+//  Created by Nastya Bekesheva on 22.02.2022.
+//
+
+#include <iostream>
+#include <vector>
+#include "matrix.hpp"
+using namespace std;
+
+template<typename T>
+Matrix<T>::Matrix(){}
+
+template<typename T>
+Matrix<T>::Matrix(int _rows, int _cols){
+    this->_rows = _rows;
+    this->_cols = _cols;
+    
+    _matrix = new T*[_rows];
+    for(int i = 0; i < _rows; i++){
+        _matrix[i] = new T[_cols];
+        
+    }
+
+    for(int i = 0; i < _rows; i++){
+        for(int j = 0; j < _cols; j++){
+            _matrix[i][j] = NULL;
+        }        
+    }
+}
+
+template<typename T>
+Matrix<T>::~Matrix<T>(){
+    for(int i = 0; i < _rows; i++){
+//        delete[] _matrix[i];
+    }
+//    delete[] _matrix;
+    cout << "Object has been deleted" << endl;
+}
+
+template<typename T>
+Matrix<T>::Matrix(const Matrix<T>& other){
+    this->_rows = other._rows;
+    this->_cols = other._cols;
+    this->_matrix = other._matrix;
+}
+
+template<typename T>
+const int Matrix<T>::row(){
+    return this->_rows;
+}
+
+template<typename T>
+const int Matrix<T>::col(){
+    return this->_cols;
+}
+
+template<typename T>
+void Matrix<T>::getSize(){
+    int size = _rows * _cols;
+    cout << "Size of your matrix: " << size << endl;
+}
+
+template<typename T>
+void Matrix<T>::getElement(unsigned int value){
+    bool temp = false;
+    for(auto i = 0; i < _rows; i++){
+        for(auto j = 0; j < _cols; j++){
+            if(_matrix[i][j] == value){
+                cout << "Element " << value << " found at row: " << i << " col: " << j << endl;
+                temp = true;
+            }
+        }
+    }
+    if(temp == false){
+        cout << "No such element" << endl;
+    }
+}
+
+template<typename T>
+ostream & operator << (ostream &out, const Matrix<T> &matrix){
+    for(auto i = 0; i < matrix._rows; i++){
+        for(auto j = 0; j < matrix._cols; j++){
+            out << matrix(i, j) << " ";
+        }
+        out << endl;
+    }
+    return out;
+}
+
+template<typename T>
+istream & operator >> (istream &in,  Matrix<T> &matrix){
+    for(auto i = 0; i < matrix.row(); i++){
+        for(auto j = 0; j < matrix.col(); j++){
+            in >> matrix(i, j);
+        }
+    }
+    return in;
+}
+
+template<typename T>
+T& Matrix<T>::operator()(const unsigned int& i, const unsigned int& j) {
+    return this->_matrix[i][j];
+}
+
+template<typename T>
+const T& Matrix<T>::operator()(const unsigned int& i, const unsigned int& j) const {
+    return this->_matrix[i][j];
+}
+
+template<typename T>
+Matrix<T>& Matrix<T>::operator=(const Matrix<T> & other){
+    
+    this->_rows = other._rows;
+    this->_cols = other._cols;
+    this->_matrix = other._matrix;
+        
+    return *this;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator+(const Matrix<T> & other){
+
+    Matrix<T> result(_rows, _cols);
+    if(this->_rows == other._rows && this->_cols == other._cols){
+        for(auto i = 0; i < _rows; i++){
+            for(auto j = 0; j < _cols; j++){
+
+                result(i, j) = _matrix[i][j] + other(i, j);
+            }
+        }
+        return result;
+    }
+    else{
+        cout << "Error" << endl;
+    }
+    return Matrix<T> ();
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator-(const Matrix<T> & other){
+
+    Matrix<T> result(_rows, _cols);
+    if(this->_rows == other._rows && this->_cols == other._cols){
+        for(auto i = 0; i < _rows; i++){
+            for(auto j = 0; j < _cols; j++){
+
+                result(i, j) = _matrix[i][j] - other(i, j);
+            }
+        }
+        return result;
+    }
+    else{
+        cout << "Error" << endl;
+    }
+    return Matrix<T> ();
+}
+
+
+template<typename T>
+Matrix<T> Matrix<T>::operator*(const Matrix<T> & other){
+    
+    Matrix<T> result(_rows, _cols);
+    if(this->_rows == other._rows && this->_cols == other._cols){
+        for(auto i = 0; i < _rows; i++){
+            for(auto j = 0; j < _cols; j++){
+                for(auto k = 0; k < _cols; k++){
+                    result(i, j) = result(i, j) + _matrix[i][k] * other(k, j);
+                }
+            }
+        }
+        return result;
+    }
+    else{
+        cout << "Error" << endl;
+    }
+    return result;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator+(const T& value){
+    Matrix<T> result(_rows, _cols);
+    for(int i = 0; i < _rows; i++){
+        for(int j = 0; j < _cols; j++){
+            result(i, j) = _matrix[i][j] + value;
+        }
+    }
+    return result;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator-(const T& value){
+    Matrix<T> result(_rows, _cols);
+    for(int i = 0; i < _rows; i++){
+        for(int j = 0; j < _cols; j++){
+            result(i, j) = _matrix[i][j] - value;
+        }
+    }
+    return result;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator*(const T& value){
+    Matrix<T> result(_rows, _cols);
+    for(int i = 0; i < _rows; i++){
+        for(int j = 0; j < _cols; j++){
+            result(i, j) = _matrix[i][j] * value;
+        }
+    }
+    return result;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::operator/(const T& value){
+    Matrix<T> result(_rows, _cols);
+    for(int i = 0; i < _rows; i++){
+        for(int j = 0; j < _cols; j++){
+            result(i, j) = _matrix[i][j] / value;
+        }
+    }
+    return result;
+}
+
+
+template<typename T>
+int NegMatrix<T>::accessItem(int i, int j){
+    if (abs(i) <= this->_rows && abs(j) <= this->_cols){
+        return this->_matrix[this->_rows + i][this->_cols + j];
+    }
+    else{
+        cout << "Incorrect index set";
+        return 0;
+    }
+}
+
+
+
