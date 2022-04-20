c4932f9ad413e0e33281bf49d443250b8cce31bb
nastyabekesheva <nastya.bekesheva@gmail.com>
2022-04-20 15:16:05 +0300

    BFS

--- Graph/Graph/main.swift
+++ Graph/Graph/main.swift
@@ -17,12 +17,17 @@ class Node{
     func getName() -> String {
         return self.name
     }
-    
+}
+
+extension Node: Equatable {
+    static func ==(_ nodeOne: Node, _ nodeTwo: Node) -> Bool {
+        return nodeOne.name == nodeTwo.name
+    }
 }
 
 class Graph{
     private var nodes: [Node] = []
-    private var edges: [(nodeOne: Node, nodeTwo: Node)] = []
+    var edges: [(nodeOne: Node, nodeTwo: Node)] = []
     
     init(nodes: [Node]){
         self.nodes = nodes
@@ -70,19 +75,71 @@ class Graph{
         }
     }
     
-    func DFS(){
-        
+    func DFS(startNode: Node) -> String{
+        var dfs = ""
+        var stack = [startNode]
+        var visited: [Node] = []
+
+        while !stack.isEmpty{
+            var v = stack.popLast()
+            
+            if !visited.contains{ $0 === v }{
+
+                for edge in self.edges{
+                    if edge.nodeOne == v && !visited.contains{ $0 === edge.nodeTwo }{
+                        stack.append(edge.nodeTwo)
+                        
+                    }
+                    else if edge.nodeTwo == v && !visited.contains{ $0 === edge.nodeOne  }{
+                        stack.append(edge.nodeOne)
+                    }
+                }
+                visited.append(v!)
+            }
+            dfs += "\(v!.getName()) "
+        }
+        return dfs
     }
     
+    func BFS(startNode: Node) -> String{
+        var bfs = ""
+        var queqe = [startNode]
+        var visited: [Node] = []
+        
+        while !queqe.isEmpty{
+            var v = queqe.removeFirst()
+            bfs += "\(v.getName()) "
+            
+            for edge in self.edges {
+                if edge.nodeOne == v && !visited.contains{ $0 === edge.nodeTwo }{
+                    queqe.append(edge.nodeTwo)
+                    
+                }
+                else if edge.nodeTwo == v && !visited.contains{ $0 === edge.nodeOne  }{
+                    queqe.append(edge.nodeOne)
+                }
+            }
+            
+            visited.append(v)
+        }
+        
+        return bfs
+    }
+
 }
 
 let node1 = Node(name: "1")
 let node2 = Node(name: "2")
 let node3 = Node(name: "3")
+let node4 = Node(name: "4")
+let node5 = Node(name: "5")
 
-var g = Graph(nodes: [node1, node2, node3])
-g.printNodes()
+var g = Graph(nodes: [node1, node2, node3, node4, node5])
 g.addEdge(nodeOne: node1, nodeTwo: node2)
-g.addEdge(nodeOne: node3, nodeTwo: Node(name: "4"))
+g.addEdge(nodeOne: node3, nodeTwo: node4)
+g.addEdge(nodeOne: node2, nodeTwo: node5)
+g.addEdge(nodeOne: node1, nodeTwo: node4)
 
-dump(g)
+print(g.DFS(startNode: node1))
+print(g.BFS(startNode: node1))
+
