27833380408ce9fb064fe99ff6eb53b587c973b7
nastyabekesheva <nastyabekesheva@gmail.com>
2022-09-26 17:10:21 +0300

    lab1 updates

--- DB/lab1.sql
+++ DB/lab1.sql
@@ -1,41 +1,22 @@
-CREATE TABLE "group" (
+CREATE TABLE subject (
+    Id int NOT NULL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE
+);
+
+CREATE TABLE students_group (
     Id int NOT NULL PRIMARY KEY,
     name VARCHAR(5) UNIQUE,
     course INTEGER,
     semester INTEGER
 );
 
-CREATE TABLE subject (
+CREATE TABLE student (
     Id int NOT NULL PRIMARY KEY,
-    name VARCHAR(255) UNIQUE
-);
-
-CREATE TABLE gender (
-    Id int NOT NULL PRIMARY KEY,
-    name VARCHAR(255) UNIQUE
-);
-
-CREATE table student (
-    Id int NOT NULL PRIMARY KEY,
-    last_name VARCHAR(255),
-    first_name VARCHAR(255),
-    middle_name VARCHAR(255),
-    gender INTEGER,
+    last_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    middle_name VARCHAR(255) NOT NULL,
+    sex VARCHAR(255) UNIQUE,
     birthday DATE,
     address VARCHAR(255),
-    "group" INTEGER
-);
-
-CREATE TABLE work_type (
-    Id int NOT NULL PRIMARY KEY,
-    name VARCHAR(255) UNIQUE
-);
-
-CREATE TABLE progress (
-    student INTEGER,
-    subject INTEGER,
-    score INTEGER,
-    hours REAL,
-    work_type INTEGER,
-    PRIMARY KEY (student, subject, work_type)
+    students_group INTEGER REFERENCES students_group(Id)
 );
--- DB/test.sql
+++ DB/test.sql
@@ -1,34 +1,13 @@
 USE master
 GO
+
 IF NOT EXISTS (
  SELECT name
  FROM sys.databases
- WHERE name = N'TutorialDB'
+ WHERE name = N'StudentsDB'
 )
- CREATE DATABASE [TutorialDB];
+ CREATE DATABASE [StudentsDB];
 GO
 IF SERVERPROPERTY('ProductVersion') > '12'
- ALTER DATABASE [TutorialDB] SET QUERY_STORE=ON;
+ ALTER DATABASE [StudentsDB] SET QUERY_STORE=ON;
 GO
-
-IF OBJECT_ID('dbo.Customers', 'U') IS NOT NULL
- DROP TABLE dbo.Customers;
-GO
--- Create the table in the specified schema
-CREATE TABLE dbo.Customers
-(
- CustomerId int NOT NULL PRIMARY KEY, -- primary key column
- Name nvarchar(50) NOT NULL,
- Location nvarchar(50) NOT NULL,
- Email nvarchar(50) NOT NULL
-);
-GO
-INSERT INTO dbo.Customers
- ([CustomerId], [Name], [Location], [Email])
-VALUES
- ( 1, N'Orlando', N'Australia', N''),
- ( 2, N'Keith', N'India', N'keith0@adventure-works.com'),
- ( 3, N'Donna', N'Germany', N'donna0@adventure-works.com'),
- ( 4, N'Janet', N'United States', N'janet1@adventure-works.com')
-GO
-SELECT * FROM dbo.Customers;
